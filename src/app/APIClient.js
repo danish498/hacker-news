6d99a314b22da5e0c1a352e18be7054d1dd75282
daanish noor <daanish629@gmail.com>
2022-11-21 01:30:20 +0530

    http-https

--- src/app/APIClient.js
+++ src/app/APIClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://hn.algolia.com/api/v1/search";
+export const BASE_URL = "https://hn.algolia.com/api/v1/search";
 
 const makeRequestCreator = () => {
   let token;
